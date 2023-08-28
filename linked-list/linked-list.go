f753e845cb8393e2730116bedb715bd034e6fc42
José Sousa <zpedro_s@hotmail.com>
2023-08-28 01:11:54 +0100

    ll

--- linked-list/linked-list.go
+++ linked-list/linked-list.go
@@ -1,12 +1,19 @@
-package linkedlist
+package main
 
 import "fmt"
 
-struct linked-list{
-
+type linkedList[T any] struct {
+	head *T
+	val  T
+	next *T
 }
 
-func main()
-{
-	
+func main() {
+	fmt.Print("Hey linked list")
+	var ll linkedList[int]
+	var head = 2
+	ll.head = &head
+	ll.val = head
+	ll.next = nil
+	fmt.Print(ll.head)
 }
